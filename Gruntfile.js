18fa39d4a886769712e71bcf0aaef02fa0023e54
Umherirrender <umherirrender_de.wp@web.de>
2020-09-12 13:44:04 +0200

    build: Use existing eslint also for json linting
    Bug: T220036
    Change-Id: I8ea4f4cac5390df3c8b0aa380428093ca50fdef2

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,31 +9,23 @@ module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 
 	grunt.initConfig( {
 		banana: conf.MessagesDirs,
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		},
 		eslint: {
 			options: {
 				cache: true
 			},
 			all: [
-				'**/*.js',
+				'**/*.{js,json}',
 				'!node_modules/**',
 				'!vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'lint', [ 'eslint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'lint', [ 'eslint', 'banana' ] );
 	grunt.registerTask( 'test', [ 'lint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
